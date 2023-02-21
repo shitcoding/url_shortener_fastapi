4fca98a9c9ae063e4259ad19b8fda60a9b84b217
shitcoding <shitcoder@pm.me>
2023-02-21 18:33:36 +0300

    refactoring: move creation of shortened url to separate module

---
+++ shortener_app/crud.py
@@ -0,0 +1,25 @@
+from sqlalchemy.orm import Session
+
+from . import models, schemas, utils
+
+
+def get_db_url_by_key(db: Session, url_key: str) -> models.URL:
+    return (
+        db.query(models.URL)
+        .filter(models.URL.key == url_key, models.URL.is_active)
+        .first()
+    )
+
+
+def create_db_url(db: Session, url: schemas.URLBase) -> models.URL:
+    key = utils.create_unique_random_key(db)
+    secret_key = f"{key}_{utils.create_random_key(length=8)}"
+    db_url = models.URL(
+        target_url=url.target_url,
+        key=key,
+        secret_key=secret_key,
+    )
+    db.add(db_url)
+    db.commit()
+    db.refresh(db_url)
+    return db_url
