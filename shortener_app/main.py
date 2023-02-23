77ca899e6dc062fecdc7b9d0c8641891ea662335
shitcoding <shitcoder@pm.me>
2023-02-23 12:33:26 +0300

    implement admin url info endpoint

--- shortener_app/main.py
+++ shortener_app/main.py
@@ -67,3 +67,19 @@ def forward_to_target_url(
         return RedirectResponse(db_url.target_url)
     else:
         raise_not_found(request)
+
+
+@app.get(
+    "/admin/{secret_key}",
+    name="administration info",
+    response_model=schemas.URLInfo,
+)
+def get_url_info(
+    secret_key: str, request, Request, db: Session = Depends(get_db)
+):
+    if db_url := crud.get_db_url_by_secret_key(db, secret_key=secret_key):
+        db_url.url = db_url.key
+        db_url.admin_url = db_url.secret_key
+        return db_url
+    else:
+        raise_not_found(request)
